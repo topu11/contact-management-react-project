8636bc1e0200fe2353455b998eb2a8516f551780
topu11 <touhidulislam256@gmail.com>
2023-07-17 08:32:49 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import { v4 as uuid } from "uuid";
+import Header from './components/Header';
+import ContactList from './components/ContactList';
+import AddContact from './components/AddContact';
 
 function App() {
+  const LOCAL_STORAGE_KEY='contactList';
+ const [contacts,setcontacts]=useState( JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []);
+
+ const addContactHandler = (contact) => {
+  
+  setcontacts([...contacts, { id: uuid(), ...contact }]);
+  
+};
+    useEffect(()=>{
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    },[contacts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} />
     </div>
   );
 }
---
+++ src/components/AddContact.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class AddContact extends React.Component 
+{
+    state={
+        name:'',
+        email:''
+    };
+    
+    add=(e)=>{
+        e.preventDefault()
+         if(this.state.name === "" || this.state.email==="")
+         {
+            alert('please insert data');
+            return;
+         }
+         this.props.addContactHandler(this.state);
+         this.setState({name:'',email:''});
+    }
+
+  render(){
+    return(
+        <div className="ui main">
+            <h2>Add Contact</h2>
+            <form className="ui form" onSubmit={this.add}>
+                <div className="field">
+                    <label>Name</label>
+                    <input type="text" value={this.state.name} onChange={(e)=>{this.setState({name:e.target.value})}} />
+                </div>
+                <div className="field">
+                    <label>Name</label>
+                    <input type="email" value={this.state.email} onChange={(e)=>{this.setState({email:e.target.value})}} />
+                </div>
+                <button className="ui button blue">Add</button>
+            </form>
+        </div>
+    )
+  }
+}
+export default AddContact;
